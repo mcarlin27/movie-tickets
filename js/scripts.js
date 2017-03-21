5445bb8c2c139fc54770d774a27725fdb5439164
Benjamin deRochemont and Marilyn Carlin <marilyn@>
2017-03-20 19:41:47 -0700

    connect front end input and run prototype method to calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,39 @@
 // business logic
-function ticket(movie, age, time) {
-  this.movie = movie;
+function Ticket(name, age, time) {
+  this.name = name;
   this.age = age;
   this.time = time;
 }
 
-ticket.prototype.ticketPrice = function() {
-  var price;
+Ticket.prototype.ticketPrice = function(age, time) {
   if (age === "12 or under") {
-    price = 500;
+    this.price = 500;
   } else if (age === "12 to 55") {
-    price = 10;
+    this.price = 10;
   } else {
-    price = 8;
+    this.price = 8;
   }
+
   if (time < 17) {
-    return (price - 2);
+    this.price = this.price - 2;
   } else if ((time > 17) && (time < 19)){
-    return (price + 3);
-  } else {
-    return price;
+    this.price = this.price + 3;
   }
 }
 
 // front end logic
-$("form#tickets").submit(function(event) {
-  event.preventDefault();
+$(document).ready(function() {
+  $("form#tickets").submit(function(event) {
+    event.preventDefault();
+    var name = $('#movie').val();
+    var time = parseInt($('#time').val());
+    var age = $('#age').val();
+    var myMovie = new Ticket(name, age, time);
+    console.log(myMovie.ticketPrice());
+    console.log(myMovie);
 
-  var time = parseInt($('#time').val());
-  var age = $('#age').val();
+
+
+  });
 
 });
