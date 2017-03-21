051d58ec83e815189384e3a4493118a4d5d49b45
Benjamin deRochemont and Marilyn Carlin <marilyn@>
2017-03-20 20:52:51 -0700

    create option to add aditional tickets based on age input. Program does not run as intended.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,61 @@
 // business logic
-function Ticket(name, age, time) {
-  this.name = name;
-  this.age = age;
+function Ticket(name, time) {
+  this.movieName = name;
+  this.age = [];
   this.time = time;
+  this.price;
 }
 
-Ticket.prototype.ticketPrice = function(age, time) {
-  if (age === "12 or under") {
-    this.price = 500;
-  } else if (age === "12 to 55") {
-    this.price = 10;
-  } else {
-    this.price = 8;
-  }
+Ticket.prototype.ticketPrice = function(age) {
+  ageArray = this.age;
+  ageArray.forEach(function(age) {
+    var price;
+    if (age === 1) {
+      price = 500;
+    } else if (age === 2) {
+      price = 10;
+    } else {
+      price = 8;
+    }
 
-  if (time < 17) {
-    this.price = this.price - 2;
-  } else if ((time > 17) && (time < 19)){
-    this.price = this.price + 3;
-  }
+    if (time < 17) {
+      price = price - 2;
+    } else if ((time > 17) && (time < 19)){
+      price = price + 3;
+    }
+    this.price += price;
+  });
+  console.log(this.price);
 }
 
 // front end logic
 $(document).ready(function() {
+  $("#add-ticket").click(function() {
+    $(".new-input").append('<div class="form-group">' +
+                              '<label for="age">Additional Ticket Age:</label>' +
+                              '<select class="age">' +
+                                '<option value="1">12 or under</option>' +
+                                '<option value="2">12 to 55</option>' +
+                                '<option value="3">55+</option>' +
+                                '<option value="4">Miltary Discount</option>' +
+                              '</select>' +
+                            '</div>')
+  });
+
   $("form#tickets").submit(function(event) {
     event.preventDefault();
     var name = $('#movie').val();
     var time = parseInt($('#time').val());
-    var age = $('#age').val();
-    var myMovie = new Ticket(name, age, time);
-    console.log(myMovie.ticketPrice());
+    var age = parseInt($('.age').val());
+    var myMovie = new Ticket(name, time);
+    $('#ticket-price').text(myMovie.price);
+    $('#result').show();
+    $('.age').each(function(){
+      myMovie.age.push(age);
+    });
+    myMovie.ticketPrice();
     console.log(myMovie);
+    debugger;
 
 
 
